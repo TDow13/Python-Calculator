a160043eabce1a206adff3ddf7cfef930a5146a5
TDow13 <Tylerwdow@gmail.com>
2024-09-09 01:19:49 -0400

    Completed the print of the calculator out

---
+++ PTUI.py
@@ -0,0 +1,26 @@
+
+
+def start_calculator():
+    calculator_print()
+
+
+
+
+
+
+
+#
+def calculator_print():
+    print("----------\n")
+    print("|       0|\n")
+    print("----------\n")
+    print("|7 8 9 % |\n")
+    print("|4 5 6 * |\n")
+    print("|1 2 3 + |\n")
+    print("|0 C . - |\n")
+    print("----------\n")
+
+
+if __name__ == '__main__':
+    start_calculator()
+    
